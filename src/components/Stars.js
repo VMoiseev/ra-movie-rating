92fa24a7c1d66bfe09675f2b0a5cea474ca704f4
VMoiseev <e175@inbox.ru>
2023-07-09 01:08:17 +0500

    task is ready

---
+++ src/components/Stars.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Star from "./Star";
+import PropTypes from "prop-types";
+
+function Stars (props) {
+  const { count } = props;
+
+  if (count < 1 || count > 5 || isNaN(count)) return null;
+  
+  const allStars = [];
+
+  for (let i = 1; i <= count; i += 1) {
+    allStars.push(i)
+  }
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {allStars.map(item => <Star key={item} />)}
+    </ul>
+  )
+}
+
+Stars.propTypes = {
+  count: PropTypes.number
+}
+
+Stars.defaultProps = {
+  count: 0
+};
+
+export default Stars;
